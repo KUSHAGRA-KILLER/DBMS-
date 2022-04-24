485ca1560ec51222047dbbb48c2208a1f820ee9d
Manoj Vaishnav <87054566+manojvaishnav@users.noreply.github.com>
2022-04-24 18:23:02 +0530

    Add files via upload

---
+++ RegisterOption.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1" >
+        <link rel="stylesheet" href="RegisterOptionStyle.css">
+        <title>Form</title>
+    </head>
+    <body>
+        <div class="container">
+            <h2>Sign Up As</h2>
+            <div class="form-container">
+                <form class="form" action="" method="POST">
+                    <button class="btn" name="" ><a href="StudentSignUp.php">Student</a></button>
+                    <button class="btn" name=""><a href="FacultySignUp.php">Faculty</a></button> 
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
